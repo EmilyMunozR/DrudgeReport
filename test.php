8fc09351a18f2e0a42dbe6f72fb24b71cd15de5c
EmilyMunozR <emilymunoz1018@gmail.com>
2026-03-24 17:14:38 -0600

    Update test.php

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php
-$fp = fsockopen("in-v3.mailjet.com", 587, $errno, $errstr, 10);
+$fp = fsockopen("in-v3.mailjet.com", 465, $errno, $errstr, 10);
 if (!$fp) {
     echo "Error: $errstr ($errno)";
 } else {

db760a8489435d91d7283afc7ad7add880da5373
EmilyMunozR <emilymunoz1018@gmail.com>
2026-04-13 16:47:44 -0600

    Delete dashboard.php

--- dashboard.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if (!isset($_SESSION["username"]) || !isset($_SESSION["authenticated"])) {
-    header("Location: login.php");
-    exit;
-}
-
-echo "✅ Bienvenido al dashboard, " . $_SESSION["username"];
-?>
